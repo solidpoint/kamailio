5b8554ff735f70b5e28c91d8e89797e6c24ab45f
Joe Hillenbrand <joe@flowroute.com>
2013-03-26 15:14:31 -0700

    build.sh fix

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # System Requirements:
-# sudo apt-get install libevent bison flex libjansson-dev libevent-dev libxml2-dev libpcre3-dev libpq-dev
+# sudo apt-get install bison flex libjansson-dev libevent-dev libxml2-dev libpcre3-dev libpq-dev pkg-config
 
-#export INCLUDED_MODULES="siputils xmlrpc json jsonrpc-c"
-export CC_EXTRA_OPTS="-g -O0"
+export CC_EXTRA_OPTS="-g -O0" ; echo -e "\n\n\n\nWARNING: OPTIMIZATION IS TURNED OFF!\nDo not use in production.\n\n\n\n"
+
 export INCLUDED_MODULES="siputils xmlrpc json jsonrpc-c"
 export INCLUDED_GROUPS="kstandard postgres"
 export EXTRA_EXCLUDED_MODULES="mi_xmlrpc cpl mangler jabber mysql cpl-c auth_radius misc_radius avp_radius uri_radius acc_radius presence_b2b xcap osp tls oracle unixsock dbg print_lib auth_identity ldap db_berkeley db_oracle db_unixodbc perl perlvdb purple xmpp peering lcr utils presence presence_mwi presence_dialoginfo presence_xml pua pua_bla pua_dialoginfo pua_usrloc pua_xmpp xcap_client xcap_server presence_conference h350 bdb dbtext iptrtpproxy pa rls app_lua app_python xmlops"
