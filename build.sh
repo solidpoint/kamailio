513eddcd4bb62983cb22337423ebe588917457ab
Joe Hillenbrand <joe@flowroute.com>
2013-05-30 09:48:05 -0700

    Add db_jsonrpc to build script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 
 export CC_EXTRA_OPTS="-g -O0" ; echo -e "\n\n\n\nWARNING: OPTIMIZATION IS TURNED OFF!\nDo not use in production.\n\n\n\n"
 
-export INCLUDED_MODULES="siputils xmlrpc json jsonrpc-c"
+export INCLUDED_MODULES="siputils xmlrpc json jsonrpc-c db_jsonrpc"
 export INCLUDED_GROUPS="kstandard postgres"
 export EXTRA_EXCLUDED_MODULES="mi_xmlrpc cpl mangler jabber mysql cpl-c auth_radius misc_radius avp_radius uri_radius acc_radius presence_b2b xcap osp tls oracle unixsock dbg print_lib auth_identity ldap db_berkeley db_oracle db_unixodbc perl perlvdb purple xmpp peering lcr utils presence presence_mwi presence_dialoginfo presence_xml pua pua_bla pua_dialoginfo pua_usrloc pua_xmpp xcap_client xcap_server presence_conference h350 bdb dbtext iptrtpproxy pa rls app_lua app_python xmlops"
 
